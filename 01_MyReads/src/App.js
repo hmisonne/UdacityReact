ec7f466b90bbd7018ec19a94c60e5e305ae93c76
hmisonne <helene.misonne@gmail.com>
2020-04-02 10:18:26 -0700

    feat: Added router

--- 01_MyReads/src/App.js
+++ 01_MyReads/src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookSelection from './BookSelection'
+import SearchBooks from './SearchBooks'
+import { Route } from 'react-router-dom'
 
-// Data Structure Draft
 const bookshelves = [
   {
     id: 1,
@@ -19,7 +20,6 @@ const bookshelves = [
     id:3,
     name:"read",
     formatedName:'Read'
-
   }]
 
 
@@ -57,37 +57,22 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : 
-        (<BookSelection
+        <Route exact path='/' render={()=> (
+          <BookSelection
             bookshelves={bookshelves}
             bookSelection={this.state.books}
             updateBookShelfLocation={this.updateBookShelfLocation}
-          />)
-      }
+          />
+          )} />
+        <Route path='/search' render={() => (
+          <SearchBooks />
+          )}/>
       </div>
     )
   }
 }
 
 export default BooksApp
+
+
+        
