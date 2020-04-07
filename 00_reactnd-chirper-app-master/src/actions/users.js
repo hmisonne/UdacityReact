aa8b9c55fb202aba020bf21986cd5590b6e69648
hmisonne <helene.misonne@gmail.com>
2020-04-07 08:02:40 -0700

    feat: Added store

--- 00_reactnd-chirper-app-master/src/actions/users.js
+++ 00_reactnd-chirper-app-master/src/actions/users.js
@@ -3,6 +3,6 @@ export const RECEIVE_USERS = 'RECEIVE_USERS'
 export function receiveUsers(users) {
 	return {
 		type: RECEIVE_USERS,
-		tweets
+		users
 	}
 }
--- 00_reactnd-chirper-app-master/src/index.js
+++ 00_reactnd-chirper-app-master/src/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-// import reducer from './reducers'
+import reducer from './reducers'
 // import middleware from './middleware'
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+document.getElementById('root'))
