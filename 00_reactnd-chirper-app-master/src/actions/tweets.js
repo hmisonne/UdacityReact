6a5842eb476b9ef201c996cde62cefd8e6f7bb1d
hmisonne <helene.misonne@gmail.com>
2020-04-07 11:57:29 -0700

    feat: Added toggle tweet reducer

--- 00_reactnd-chirper-app-master/src/actions/tweets.js
+++ 00_reactnd-chirper-app-master/src/actions/tweets.js
@@ -1,8 +1,33 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 export function receiveTweets(tweets) {
 	return {
 		type: RECEIVE_TWEETS,
 		tweets
 	}
+}
+
+export function toggleTweets(id, authedUser, hasLiked) {
+	return {
+		type: TOGGLE_TWEET,
+		id, 
+		authedUser,
+		hasLiked
+	}
+}
+
+export function handleToggleTweets(info){
+	return (dispatch) => {
+		dispatch(toggleTweets(info))
+		return saveLikeToggle(info)
+			.catch((e) => {
+				console.warn('Error in handleToggleTweets: ',e)
+				dispatch(toggleTweets(info))
+				alert('There was an error liking the tweet. Try again')
+			})
+	}
+
 }
