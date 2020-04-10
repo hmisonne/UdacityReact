aed3452c358165d67a0eafa20ff006c36bfdaf9f
hmisonne <helene.misonne@gmail.com>
2020-04-10 11:36:50 -0700

    feat: Added Router

--- 02_would_you_rather_app/src/components/App.js
+++ 02_would_you_rather_app/src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component} from 'react';
+import React, { Component, Fragment} from 'react';
 import { handleInitialData } from '../actions/shared'
 import { connect } from 'react-redux'
 import Dashboard from './Dashboard'
 import Leaderboard from './Leaderboard'
 import NewQuestions from './NewQuestions'
 import QuestionDetails from './QuestionDetails'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
 	componentDidMount(){
@@ -12,12 +13,21 @@ class App extends Component {
 	}
 	render(){
 		return (
-		    <div className="container">
-		      <Dashboard />
-		      <Leaderboard sortedUsers={this.props.sortedUsers} users={this.props.users}/>
-		      <NewQuestions/>
-		      <QuestionDetails id='8xf0y6ziyjabvozdd253nd'/>
-		    </div>
+			<Router>
+				<Fragment>
+				    <div className="container">
+						<Route exact path='/' component={Dashboard}/>
+						<Route path='/add' component={NewQuestions}/>
+						<Route path='/questions/:id' component={QuestionDetails}/>
+				        <Route exact path='/leaderboard' render={() => (
+				          <Leaderboard
+				            sortedUsers={this.props.sortedUsers}
+				            users={this.props.users}
+				          />
+				        )} />
+				    </div>
+			    </Fragment>
+			</Router>
 		  );
 	}
   
@@ -34,3 +44,4 @@ function mapStateToProps ({ questions, authedUser, users }) {
 }
 
 export default connect(mapStateToProps)(App);
+// <QuestionDetails id='8xf0y6ziyjabvozdd253nd'/>
