8608283a6062920292bc36181e2dcaccf8bf2764
hmisonne <helene.misonne@gmail.com>
2020-04-10 09:40:35 -0700

    feat: Added actions, reducers and component to add new questions

--- 02_would_you_rather_app/src/actions/questions.js
+++ 02_would_you_rather_app/src/actions/questions.js
@@ -1,8 +1,32 @@
+import { saveQuestion } from '../utils/api'
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveQuestions(questions) {
 	return {
 		type: RECEIVE_QUESTIONS,
 		questions
+	}
+}
+
+export function addQuestion(formattedQuestion) {
+	return {
+		type: ADD_QUESTION,
+		formattedQuestion,
+	}
+}
+
+export function handleAddQuestion(optionOne, optionTwo) {
+	return (dispatch, getState) => {
+		const {authedUser} = getState()
+
+		return saveQuestion({ 
+			optionOneText: optionOne, 
+			optionTwoText: optionTwo, 
+			author:  authedUser
+		})
+		.then((question)=> dispatch(addQuestion(question)))
+
+		
 	}
 }
---
+++ 02_would_you_rather_app/src/components/NewQuestions.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddQuestion } from '../actions/questions'
+
+class NewQuestion extends Component {
+	state = {
+		optionOne: '',
+		optionTwo: '',
+	}
+
+	handleChange = (e) => {
+		const {value, name} = e.target
+		this.setState(() => ({
+			[name] : value,
+		}))
+	}
+
+	handleSubmit = () => {
+		const { dispatch } = this.props
+		const { optionOne, optionTwo } = this.state
+		dispatch(handleAddQuestion(optionOne, optionTwo))
+	}
+
+	render(){
+		return(
+			<div>
+				<h4>Create New Question</h4>
+				<div>
+					Would you rather... 
+				</div>
+				<form 
+					onSubmit={this.handleSubmit}
+					className='new-question'>
+					<input
+						value = {this.state.optionOne}
+						onChange = {this.handleChange}
+						placeholder="Enter option one text here"
+						name = 'optionOne'/>
+					<div> OR </div>
+					<input
+						value = {this.state.optionTwo}
+						onChange = {this.handleChange}
+						placeholder="Enter option two text here"
+						name = 'optionTwo'/>
+					<button>Submit</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+
+
+export default connect()(NewQuestion)
