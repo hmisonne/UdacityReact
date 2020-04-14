a7b382bdefd71277065b5f53b746066b7e874b94
hmisonne <helene.misonne@gmail.com>
2020-04-14 11:49:18 -0700

    First commit

---
+++ 00_UdaciFitness/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  componentDidMount() {
+    console.log('Before')
+    // debugger
+    console.log('After')
+  }
+  
+  render(){
+     return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+  }
+ 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
