dce8832f456a5c955a323309b7f821f2ddef15ca
hmisonne <helene.misonne@gmail.com>
2020-04-16 10:02:09 -0700

    feat: Added Navigation, bug: accessibilityRole tab

--- 00_UdaciFitness/App.js
+++ 00_UdaciFitness/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry'
@@ -5,8 +6,10 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
-// import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
-// import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
 import { purple, white } from './utils/colors'
 import {
   FontAwesome,
@@ -21,8 +24,7 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
     </View>
   )
 }
-
-
+const Tab = createMaterialTopTabNavigator();
 
 export default class App extends React.Component {
   
@@ -31,7 +33,12 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          <History/>
+           <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name="History" component={History} />
+              <Tab.Screen name="Add Entry" component={AddEntry} />
+            </Tab.Navigator>
+          </NavigationContainer>
         </View>
       </Provider>
   );
@@ -40,42 +47,46 @@ export default class App extends React.Component {
 }
 
 
-// <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-// <TabNav />
 
 
-// const Tabs = {
-//   History: {
-//     screen: History,
-//     navigationOptions: {
-//       tabBarLabel: 'History',
-//       tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-//     },
-//   },
-//   AddEntry: {
-//     screen: AddEntry,
-//     navigationOptions: {
-//       tabBarLabel: 'Add Entry',
-//       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-//     },
-//   },
-// }
-
-// const navigationOptions = {
-//   tabBarOptions: {
-//     activeTintColor: Platform.OS === 'ios' ? purple : white,
-//     style: {
-//       height: 56,
-//       backgroundColor: Platform.OS === 'ios' ? white : purple,
-//       shadowColor: 'rgba(0, 0, 0, 0.24)',
-//       shadowOffset: {
-//         width: 0,
-//         height: 3
-//       },
-//       shadowRadius: 6,
-//       shadowOpacity: 1
-//     }
-//   }
-// }
-
-// const TabNav = createAppContainer(Platform.OS === 'ios' ? createBottomTabNavigator(Tabs, navigationOptions) : createMaterialTopTabNavigator(Tabs, navigationOptions))
+// const Tabs =
+//   Platform.OS === "ios"
+//     ? createBottomTabNavigator()
+//     : createMaterialTopTabNavigator();
+//           <NavigationContainer>
+//           <Tabs.Navigator
+//             initialRouteName="AddEntry"
+//             screenOptions={({ route }) => ({
+//               tabBarIcon: ({ color, size }) => {
+//                 let icon;
+//                 if (route.name === "Add Entry") {
+//                   icon = (
+//                     <FontAwesome name="plus-square" size={size} color={color} />
+//                   );
+//                 } else if (route.name === "History") {
+//                   icon = (
+//                     <Ionicons name="ios-bookmarks" size={size} color={color} />
+//                   );
+//                 }
+//                 return icon;
+//               }
+//             })}
+//             tabBarOptions={{
+//               activeTintColor: Platform.OS === "ios" ? purple : white,
+//               style: {
+//                 height: 80,
+//                 backgroundColor: Platform.OS === "ios" ? white : purple,
+//                 shadowColor: "rgba(0, 0, 0, 0.24)",
+//                 shadowOffset: {
+//                   width: 0,
+//                   height: 3
+//                 },
+//                 shadowRadius: 6,
+//                 shadowOpacity: 1
+//               }
+//             }}
+//           >
+//             <Tabs.Screen name="Add Entry" component={AddEntry} />
+//             <Tabs.Screen name="History" component={History} />
+//           </Tabs.Navigator>
+//         </NavigationContainer>
