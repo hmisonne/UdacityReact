45a766633499d776a0a2391548a761507ed5e652
hmisonne <helene.misonne@gmail.com>
2020-04-15 19:25:25 -0700

    feat: Added calendar and fixed version issue

--- 00_UdaciFitness/components/History.js
+++ 00_UdaciFitness/components/History.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
-import { Text, TouchableOpacity, StyleSheet, View } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 import { receiveEntries, addEntry } from '../actions'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
+import { AppLoading} from 'expo'
 
 class History extends Component {
+   state = {
+    ready: false,
+  }
   componentDidMount() {
     const {dispatch} = this.props
     fetchCalendarResults()
@@ -18,23 +26,81 @@ class History extends Component {
           }))
         }
       })
+      .then(()=> this.setState(()=>({
+        ready: true,
+      })))
     }
-    
-  render() {
-    return (
-      <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+    renderItem = ({today, ...metrics}, formattedDate, key) =>(
+      <View style={styles.item}>
+      {today
+          ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+          >
+               <MetricCard date={formattedDate} metrics={metrics} />
+          </TouchableOpacity>}
+        </View>
+      )
+    renderEmptyDate(formattedDate) {
+      return (
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
+      )
+      
+    }
+  render() {
+    const { ready } = this.state
+    const {entries} = this.props
+     if (ready === false) {
+      return <AppLoading />
+    }
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
   
 }
 
+
+
+
+
 const styles = StyleSheet.create({
-  reset: {
-    
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
   }
-}) 
+})
 
 function mapStateToProps (entries) {
   return {
