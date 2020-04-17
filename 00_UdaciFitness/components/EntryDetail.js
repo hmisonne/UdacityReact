c966a55e903aedf79313ea89b24ffd8f6d0c286a
hmisonne <helene.misonne@gmail.com>
2020-04-16 17:38:49 -0700

    feat: Added title to entry details page

--- 00_UdaciFitness/components/EntryDetail.js
+++ 00_UdaciFitness/components/EntryDetail.js
@@ -2,7 +2,21 @@ import React, {Component} from 'react'
 import {Text, View} from 'react-native'
 
 class EntryDetail extends Component {
+    setTitle = (entryId) => {
+        if (!entryId) return;
+
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+
+        this.props.navigation.setOptions({
+            title: `${month}/${day}/${year}`
+        });
+    };
+
     render() {
+        const {entryId} = this.props.route.params;
+        this.setTitle(entryId);
         return (
             <View>
                 <Text>Entry Detail - {JSON.stringify(this.props.route.params.entryId)}</Text>
