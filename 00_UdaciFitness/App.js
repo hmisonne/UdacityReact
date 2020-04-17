f7801085492740446432702b3f903d19244cbe24
hmisonne <helene.misonne@gmail.com>
2020-04-16 17:23:55 -0700

    style: created separate component for tab

--- 00_UdaciFitness/App.js
+++ 00_UdaciFitness/App.js
@@ -1,21 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
-import AddEntry from './components/AddEntry'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import History from './components/History'
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+
 import EntryDetail from './components/EntryDetail'
+import TabNav from './components/TabNav'
 import { purple, white } from './utils/colors'
-import {
-  FontAwesome,
-  Ionicons,
-} from "@expo/vector-icons";
+
 import Constants from 'expo-constants';
 
 function UdaciStatusBar ({backgroundColor, ...props}) {
@@ -26,46 +21,7 @@ function UdaciStatusBar ({backgroundColor, ...props}) {
   )
 }
 
-const RouteConfigs = {
-  History:{
-    name: "History",
-    component: History,
-    options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />, title: 'History'}
-  }, 
-  AddEntry:{
-    component: AddEntry,
-    name: "Add Entry",
-    options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'Add Entry'}
-  }
-}
 
-const TabNavigatorConfig = {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === "ios" ? purple : purple,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === "ios" ? white : white,
-      shadowColor: "rgba(0, 0, 0, 0.24)",
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-  };
-const Tab = createBottomTabNavigator();
-
-const TabNav = () =>(
-  <Tab.Navigator {...TabNavigatorConfig}>
-      <Tab.Screen {...RouteConfigs['History']} />
-      <Tab.Screen {...RouteConfigs['AddEntry']} />
-  </Tab.Navigator>
-)
 
 // Config for StackNav
 const StackNavigatorConfig = {
---
+++ 00_UdaciFitness/components/TabNav.js
@@ -0,0 +1,54 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import History from './History'
+import AddEntry from './AddEntry'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { purple, white } from '../utils/colors'
+import {
+  FontAwesome,
+  Ionicons,
+} from "@expo/vector-icons";
+
+const RouteConfigs = {
+  History:{
+    name: "History",
+    component: History,
+    options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />, title: 'History'}
+  }, 
+  AddEntry:{
+    component: AddEntry,
+    name: "Add Entry",
+    options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'Add Entry'}
+  }
+}
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : purple,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : white,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+  };
+const Tab = createBottomTabNavigator();
+
+const TabNav = () =>(
+  <Tab.Navigator {...TabNavigatorConfig}>
+      <Tab.Screen {...RouteConfigs['History']} />
+      <Tab.Screen {...RouteConfigs['AddEntry']} />
+  </Tab.Navigator>
+)
+
+export default TabNav
