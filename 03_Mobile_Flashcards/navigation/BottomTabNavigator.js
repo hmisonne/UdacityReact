97ce72b2c7011fa9f7be563657b827b5c2c38d83
hmisonne <helene.misonne@gmail.com>
2020-04-24 09:30:26 -0700

    fix: removed auto prettify on routename

--- 03_Mobile_Flashcards/navigation/BottomTabNavigator.js
+++ 03_Mobile_Flashcards/navigation/BottomTabNavigator.js
@@ -37,7 +37,7 @@ export default function BottomTabNavigator({ navigation, route }) {
 }
 
 function getHeaderTitle(route) {
-    const routeName = route.state ? .routes[route.state.index] ? .name ? ? INITIAL_ROUTE_NAME;
+    const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
     switch (routeName) {
         case 'Home':
